e94d45347067018d1aeac3bb413cafce09864be6
Zafar Ahmad <zahmad@iesjoandaustria.org>
2019-04-24 19:30:36 +0200

    Zafar.Modificaciones

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,19 +5,19 @@
       
    <!--Hasta aqui solo el header -->
    <!-- Contenido que ofrecemos Aqui podemos enviar a cada ruta que queramos  -->
-   <img class="ImagePortada" src="media/ImagenPortada.png" alt="" />
+   <img class="ImagePortada" src="media/bg.jpg" alt="" />
    <section id="main-content">
          <article>
             <header>
                <h1>Aqui como quieran el titulo o nada </h1>
             </header>
             <div class="content">
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+               <p id="content-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+               <p id="content-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-               <p>
+               <p id="content-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+               <p id="content-p">
                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur officia rem deleniti modi ipsa optio, unde iste! Ipsum sed veniam hic iure maiores et aperiam excepturi omnis adipisci cumque. Expedita.
                </p>
             </div>
@@ -26,7 +26,39 @@
    
    </section>
    <footer id="main-footer">
-         <p>Copyright &copy; 2019 <a href="#">No copiar</a></p>
+      <div class="row">
+         <div class="col-4">
+            <ul>
+               <li>
+                  <span class="footer.list">1</span>
+                  <span class="footer.list">2</span>
+                  <span class="footer.list">3</span>
+                  <span class="footer.list">4</span>
+               </li>
+            </ul>
+         </div>
+         <div class="col-4">
+            <ul>
+               <li>
+                  <span class="footer.list">1</span>
+                  <span class="footer.list">2</span>
+                  <span class="footer.list">3</span>
+                  <span class="footer.list">4</span>
+               </li>
+            </ul>
+         </div>
+         <div class="col-4">
+            <ul>
+               <li>
+                  <span class="footer.list">1</span>
+                  <span class="footer.list">2</span>
+                  <span class="footer.list">3</span>
+                  <span class="footer.list">4</span>
+               </li>
+            </ul>
+         <div>
+      </div>
+      <p id="footer-p" >Copyright &copy; 2019 <a href="#">No copiar</a></p>
    </footer> 
 </div>
 
