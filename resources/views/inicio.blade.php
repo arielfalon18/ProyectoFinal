ce8a473cd71be5fa35912e88ed57912df0805e25
Zafar Ahmad <zahmad@iesjoandaustria.org>
2019-04-25 16:31:41 +0200

    Styles Header and body

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,28 +5,28 @@
       
    <!--Hasta aqui solo el header -->
    <!-- Contenido que ofrecemos Aqui podemos enviar a cada ruta que queramos  -->
-   <img class="ImagePortada" src="media/bg.jpg" alt="" />
+   <img class="ImagePortada" src="media/prueba/1.jpg" alt="" />
    <section id="main-content">
          <article>
             <header>
-               <h1>Aqui como quieran el titulo o nada </h1>
+               <h1>Informacion</h1>
             </header>
-            <div class="content">
-               <p id="content-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-               
-               <p id="content-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-               
-               <p id="content-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-               <p id="content-p">
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur officia rem deleniti modi ipsa optio, unde iste! Ipsum sed veniam hic iure maiores et aperiam excepturi omnis adipisci cumque. Expedita.
-               </p>
+            <div class="container">
+               <div class="row">
+                  <div class="col-6">a</div>
+                  <div class="col-6">a</div>
+               </div>
+               <div class="row">
+                  <div class="col-6">a</div>
+                  <div class="col-6">a</div>
+               </div>
             </div>
             
          </article> 
    
    </section>
    <footer id="main-footer">
-      <div class="row">
+      <div class="row footer-row">
          <div class="col-4">
             <ul>
                <li>
@@ -56,7 +56,7 @@
                   <span class="footer.list">4</span>
                </li>
             </ul>
-         <div>
+         </div>
       </div>
       <p id="footer-p" >Copyright &copy; 2019 <a href="#">No copiar</a></p>
    </footer> 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,17 +1,15 @@
 <header id="main-header" >
-            <a id="logo-header" href="inicio">
-                <!-- Nota poner el nombre de la aplicacion o un logo  -->
-                <img id="main-header-logo" src="media/logo/logo.png" alt="">
-                
-            </a> 
-            <!-- Navgador de la derecha lo podemos poner en el centro o dejarlo ahy  -->
-            <nav>
-                <ul>
-                    <li><a href="/inicio">Inicio</a></li>
-                    <li><a href="/Nosotros">Acerca de nosotros</a></li>
-                    <li><a href="/Contactos">Contacto</a></li>
-                    <li><a href="/registrarse">Acceso</a></li>
-                </ul>
-            </nav>
+    <div class="div-logo">
+        <a id="logo-header" href="inicio"><img id="main-header-logo" src="media/logo/logo-transparent.png" alt=""></a>
+    </div> 
+    <!-- Navgador de la derecha lo podemos poner en el centro o dejarlo ahy  -->
+    <nav>
+        <ul>
+            <li><a href="/inicio">Inicio</a></li>
+            <li><a href="/Nosotros">Acerca de nosotros</a></li>
+            <li><a href="/Contactos">Contacto</a></li>
+            <li><a href="/registrarse">Acceso</a></li>
+        </ul>
+    </nav>
     
 </header>
