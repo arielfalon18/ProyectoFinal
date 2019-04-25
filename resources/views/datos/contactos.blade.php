727851582d96acda087634d152a56409747687fb
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-04-25 18:14:54 +0200

    style contacto

--- resources/views/datos/contactos.blade.php
+++ resources/views/datos/contactos.blade.php
@@ -1,8 +1,31 @@
 @extends('layouts.master')
 @section('content')
     <h1 id="contactanos">CONTÁCTANOS</h1>
-    <div>
-        <p id="llamanos">Llamanos</p>
+    <div id="info">
+        <p><strong>Llamanos:</strong> 666 666 666</p>
+        <p><strong>Localízanos:</strong> Carrer de la Selva de Mar, 211, 08020 Barcelona</p>
+        <p>Theincidence@incidence.org</p>
     </div>
+    <div class="container">
+        <form>
+        <div class="row styloRow">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Nombre">
+            </div>
+            <div class="col">
+                <input type="number" class="form-control" placeholder="Telefono">
+            </div>
+        </div>
+        <div class="row styloRow">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Email">
+            </div>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Repite Email">
+            </div>
+        </div>
+        </form>     
+    </div>
+    
 @stop
 
