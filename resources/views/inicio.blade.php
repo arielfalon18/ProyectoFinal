e1ec07b5e51754e8d3e47c9b62820214298563ba
Zafar Ahmad <zahmad@iesjoandaustria.org>
2019-04-25 18:56:34 +0200

    Zafar Push

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.master')
 @section('content')
 
-<div>
+<div class="content">
       
    <!--Hasta aqui solo el header -->
    <!-- Contenido que ofrecemos Aqui podemos enviar a cada ruta que queramos  -->
    <img class="ImagePortada" src="media/prueba/1.jpg" alt="" />
+   <div>
+      <div class="text-center-main">The Incidence</div>
+      <div class="text-center-secondary">Gestio de incidencias para empresas</div>
+   </div>
    <section id="main-content">
          <article>
             <header>
@@ -14,13 +18,29 @@
             <div class="container">
                <div class="row">
                   <div class="col-6">
-                     <div class="div-img"></div>
+                     <div class="div-img">
+                        <img class="img-1" src="media/tickets.png" alt="">
+
+                     </div>
                   </div>
-                  <div class="col-6">a</div>
+                  <div class="col-6">
+                     <div class="div-text">
+                     <p>Hola</p>
+                     </div>
+                  </div>
                </div>
                <div class="row">
-                  <div class="col-6">a</div>
-                  <div class="col-6">a</div>
+                  <div class="col-6">
+                     <div class="div-text">
+                     <p>Hola</p>
+                     </div>
+                  
+                  </div>
+                  <div class="col-6">
+                     <div class="div-img">
+                     <img class="img-1" src="media/tickets.png" alt="">
+                     </div>
+                  </div>
                </div>
             </div>
             
