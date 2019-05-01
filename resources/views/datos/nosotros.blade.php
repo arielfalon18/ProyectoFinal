3a5e1e7924a3106c89648a58ce065de2945eded7
arielfalon18 <48770847+arielfalon18@users.noreply.github.com>
2019-05-01 21:39:07 +0200

    DB para algunos

--- resources/views/datos/nosotros.blade.php
+++ resources/views/datos/nosotros.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.master')
 @section('content')
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2992.060617944183!2d2.196934615858059!3d41.41620277926166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a3340ac86a75%3A0xf6183083d8d67bf6!2sInstitut+Joan+d&#39;Austria!5e0!3m2!1sca!2ses!4v1556738334282!5m2!1sca!2ses"
- width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div class="registro">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2992.060617944183!2d2.196934615858059!3d41.41620277926166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a3340ac86a75%3A0xf6183083d8d67bf6!2sInstitut+Joan+d&#39;Austria!5e0!3m2!1sca!2ses!4v1556738334282!5m2!1sca!2ses"
+    width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
 @stop
