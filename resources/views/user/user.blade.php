9b7d9215062e6caa252e976b122800f33cfe2ed4
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-05-02 16:35:40 +0200

    recuperado body usuario

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -2,4 +2,17 @@
 @section('content')
 <div class="usuario">
     f
+    <div class="cointainer">
+
+    <div class="container-fluid border">
+    <button><p>Crear incidencia</p></button>
+        <div class="row">
+            <div class="col-4 to-do border">
+                <div class="card1">
+                </div>
+            </div>
+            <div class="col-4 doing border">asd</div>
+            <div class="col-4 done border">asd</div>
+        </div>
+    </div>
 </div>
