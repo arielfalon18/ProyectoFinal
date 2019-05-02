c9cd91351cfaac3f475c5d84ccc7ab8e4247763b
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-05-02 16:27:04 +0200

    cambio en la vista usuario

--- resources/views/partials/navbaruser.blade.php
+++ resources/views/partials/navbaruser.blade.php
@@ -1,6 +1,6 @@
 <header id="main-header">
     <!-- Navgador de la derecha lo podemos poner en el centro o dejarlo ahy  -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,10 +10,10 @@
           <a class="nav-link nombreUser">Nombre Usuario <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link">TITULO EMPRESA</a>
+          <a class="nav-link empresa">TITULO EMPRESA</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/inicio">Cerrar sesion</a>
+          <a class="nav-link cerrarSesion" href="/inicio">Cerrar sesion</a>
         </li>
       </ul>
     </div>
--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.user')
 @section('content')
-<div class="container">
-    <h1>asdasdasdasd</h1>
-<div class="class">
-    <h1>Hola</h1>
-    <hr>
-    <h3>PRueba</h3>
+<div class="usuario">
+    f
 </div>
