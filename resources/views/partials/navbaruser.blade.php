a80fcf9064879e8f924c9edf639b59b037aac467
David Belenguer <cf17david.belenguer@iesjoandaustria.org>
2019-05-02 16:50:59 +0200

    pesado

--- resources/views/partials/navbaruser.blade.php
+++ resources/views/partials/navbaruser.blade.php
@@ -1,23 +1,13 @@
-<header id="main-header">
-    <!-- Navgador de la derecha lo podemos poner en el centro o dejarlo ahy  -->
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link nombreUser">Nombre Usuario <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link empresa">TITULO EMPRESA</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link cerrarSesion" href="/inicio">Cerrar sesion</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+<header class="header">
+  <a href="" class="logo"> <a id="logo-header" href="inicio"><img id="main-header-logo" src="media/logo/logo-transparent.png" alt=""></a></a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <ul class="menu">
+    <li><a href="/inicio">Inicio</a></li>
+    <li><a href="/Nosotros">Acerca de nosotros</a></li>
+    <li><a href="/Contactos">Contacto</a></li>
+    <li><a href="/registrarse">Accesp</a></li>
+  </ul>
 </header>
 
 
