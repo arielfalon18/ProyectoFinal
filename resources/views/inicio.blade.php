f3f386952ae24778b92f620509c5c82abb578d61
Zafar Ahmad <zahmad@iesjoandaustria.org>
2019-05-02 15:30:16 +0200

    commit zafar

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -18,12 +18,12 @@
                </header>
                <div class="container">
                   <div class="row">
-                     <div class="col-6">
+                     <div class="col-md-6">
                         <div class="div-img">
                            <img class="img-1" src="media/tickets.png" alt="">
                         </div>
                      </div>
-                     <div class="col-6">
+                     <div class="col-md-6">
 
                            <p class="text1">En TheIncidence puedes hacer que el servicio y soporte al cliente a la hora de gestionar las incidencias sea mas facil y productivo</p>
 
@@ -37,16 +37,11 @@
                   <h1>Servicio al cliente personalizado y simple</h1>
                </header>
                <div class="row">
-                  <div class="col-6">
-
-                        <p class="text2">Ofrecemos un gestor de 
-   incidencias adaptable 
-   para cualquier empresa que 
-   lo desee.</p>
-
+                  <div class="col-md-6">
+                        <p class="text2">Ofrecemos un gestor de incidencias adaptable para cualquier empresa que lo desee.</p>
                   </div>
-                  <div class="col-6 col6">
-                     <div class="div-img2">
+                  <div class="col-md-6 col6">
+                     <div class="div-img">
                         <img class="img-2 " src="media/help.png" alt="">
                      </div>
                   </div>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,9 @@
 <header id="main-header" clas="">
+
     <div class="div-logo">
         <a id="logo-header" href="inicio"><img id="main-header-logo" src="media/logo/logo-transparent.png" alt=""></a>
     </div> 
-    <!-- Navgador de la derecha lo podemos poner en el centro o dejarlo ahy  -->
+<!-- 
     <nav>
         <ul>
             <li><a href="/inicio">Inicio</a></li>
@@ -10,5 +11,24 @@
             <li><a href="/Contactos">Contacto</a></li>
             <li><a href="/registrarse">Acceso</a></li>
         </ul>
-    </nav>
+    </nav> -->
+
+<nav class=" main-header navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/inicio">Inicio</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="/Nosotros">Aserca de nosotros</a>
+      </li>
+      <li class="nav-item active" >
+        <a class="nav-link" href="/Contactos">Contacto</a>
+      </li>
+      <li class="nav-item active" >
+        <a class="nav-link" href="/registrarse">Acceso</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 </header>
